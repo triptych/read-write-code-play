3bb299f8c0ff05b05600bd3f25287a458fb21760
Andrew Wooldridge <triptych@gmail.com>
2011-10-18 16:00:41 -0700

    adding a blinky question mark

--- src/web/js/rwcp.js
+++ src/web/js/rwcp.js
@@ -18,7 +18,9 @@
                 Y.one(".play").setStyle("bottom", "-"+ parseInt(Y.one(".read").getStyle("height"))/2);
                 Y.one(".play").setStyle("right", "-"+ parseInt(Y.one(".read").getStyle("width"))/2);
 
-                Y.one(".question").on("click", function(){
+                Y.one(".question span").on("click", function(){
+                    this.addClass("colorcycle");
+
                     Y.YQL('use "https://raw.github.com/triptych/read-write-code-play/master/src/yql/rwcp.xml" as table; select * from table where type="*" ', 
                         function(r){
                             var responseArr = r.query.results.result.data.results.row
@@ -32,6 +34,7 @@
 
                 Y.one("div.close").on("click", function(){
                     Y.one("body").removeClass("showres");
+                    Y.one(".question span").removeClass("colorcycle");
                 })
 
                 Y.one("div.dismiss").on("click", function(){
